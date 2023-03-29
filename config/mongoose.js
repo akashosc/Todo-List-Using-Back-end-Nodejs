c9699b4dc8a3340a9ee51cd7b4b4d0137a48d995
akashosc <84351117+akashosc@users.noreply.github.com>
2023-03-30 02:14:48 +0530

    Main Branch

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose=require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/Todotask", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+//error
+db.on('error', function(err) { console.log(err.message); });
+
+//up and running then print the message
+db.once('open', function() {
+  
+    console.log("Successfully connected to the database");
+
+});
